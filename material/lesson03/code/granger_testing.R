a16ee6bd55a866272b9960321170ad830f7f5ce0
gherardo varando <gherardo.varando@gmail.com>
2025-02-18 10:19:28 +0100

    clean files

--- material/lesson03/code/granger_testing.R
+++
@@ -1,9 +0,0 @@
-library(lmtest)
-## Which came first: the chicken or the egg?
-data(ChickEgg)
-
-plot(ChickEgg)
-
-
-grangertest(egg ~ chicken, order = 3, data = ChickEgg)
-grangertest(chicken ~ egg, order = 3, data = ChickEgg)
